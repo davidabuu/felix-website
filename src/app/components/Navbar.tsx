4a1d909b530055600cf9741972acd2334efbceaa
davidabuu <davidabu80@gmail.com>
2023-07-20 13:57:13 +0100

    Landing Page

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useState } from "react";
+const Navbar = () => {
+  const [nav, setNav] = useState<boolean>(false);
+  const showNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="flex border justify-around items-center w-full py-6">
+      <h1 className="font-bold logo text-red-600 text-3xl text-shadow">
+        {" "}
+        Studio L'Amour
+      </h1>
+      <ul className="items-center w-[50%] justify-around hidden md:flex">
+        <a href="p#">
+          <li>Home</li>
+        </a>
+        <a href="#about">
+          <li>About</li>
+        </a>
+        <a href="#services">
+          <li>Service</li>
+        </a>
+        <a href="#blog">
+          <li>Doctors</li>
+        </a>
+        <a href="#contract">
+          <li>Contacts</li>
+        </a>
+      </ul>
+      <div
+        className="md:hidden"
+        onClick={showNav}
+      >
+        {/* {!nav ? (
+          <Menu className="text-white cursor-pointer" />
+        ) : (
+          <Time className="text-white cursor-pointer" />
+        )} */}
+      </div>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : " md:hidden absolute w-full mt-72 bg-[#2e2e37] bg-opacity-70"
+        }
+      >
+        <li className="p-2 border-b-[1px] w-full">Home</li>
+        <li className="p-2 border-b-[1px] w-full">About</li>
+        <li className="p-2 border-b-[1px] w-full">Service</li>
+        <li className="p-2 border-b-[1px] w-full">Doctors</li>
+        <li className="p-2 border-b-[1px] w-full">Contacts</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        custom: ['Noto Serif Display', 'sans-serif'], // Replace 'YourFontName' with the name you specified in the @font-face rule
       },
-    },
+    }
   },
   plugins: [],
-}
+};
